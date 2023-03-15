4050b28e90b4fbd8d549639815086947d2118406
José Alessandro Santos Santana <jose.santana088@academico.ifs.edu.br>
2023-03-14 23:29:57 -0300

    API address to heroku

--- lib/config/api_constants.dart
+++ lib/config/api_constants.dart
@@ -1,4 +1,10 @@
 class APIConstants {
-  static const String API_BACKEND_WEBSOCKET = "ws://192.168.0.111:8080";
-  static const String API_BACKEND = "http://192.168.0.111:8080";
+  // Local
+  // static const String API_BACKEND_WEBSOCKET = "ws://192.168.0.111:8080";
+  // static const String API_BACKEND = "http://192.168.0.111:8080";
+
+  // Heroku
+  static const String API_BACKEND_WEBSOCKET = "ws://chat-online.herokuapp.com";
+  static const String API_BACKEND = "https://chat-online.herokuapp.com";
+
 }
